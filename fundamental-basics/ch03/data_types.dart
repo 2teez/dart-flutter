5f81fe59693734fbd057a3cbe74432bab899f192
timothy adigun <adigun.timothy@gmail.com>
2026-08-29 23:48:36 +0100

    value and variable type in dart programming

--- fundamental-basics/ch03/data_types.dart
+++ fundamental-basics/ch03/data_types.dart
@@ -10,4 +10,13 @@ void main() {
   print(averageAge.runtimeType);
   print(averageAge.round());
   print(averageAge.toInt());
+  // exercise
+  double attendance = 0.2 * 90;
+  double homework = 0.3 * 80;
+  double exam = 0.5 * 94;
+  int grade = (attendance + homework + exam).round();
+  print(grade);
+  // type of value
+  const value = 10 / 2;
+  print(value.runtimeType);
 }
