d08e52936b93762151137840831396dc8a545776
timothy adigun <adigun.timothy@gmail.com>
2026-08-29 16:11:20 +0100

    exercise for practice in dart

--- junkz/practice_math.dart
+++ junkz/practice_math.dart
@@ -2,4 +2,10 @@ import 'dart:math';
 
 void main() {
   print(sin(45 * pi / 180));
+  print(cos(135 * pi / 180));
+  print(sqrt(2));
+  // exercise
+  var new_value = 1 / sqrt(2);
+  print(new_value);
+  print(new_value == sin(45 * pi / 180));
 }
