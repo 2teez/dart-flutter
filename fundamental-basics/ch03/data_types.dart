b939c6cf4f468f52bfba3d047935008bfa6f89e1
timothy adigun <adigun.timothy@gmail.com>
2026-08-29 23:42:16 +0100

    practice on data types

--- fundamental-basics/ch03/data_types.dart
+++ fundamental-basics/ch03/data_types.dart
@@ -1,7 +1,13 @@
 void main() {
-  const age1 = 42;
+  const num age1 = 42;
   const age2 = 21;
   print(age1);
   print(age1.runtimeType);
   print(age2.runtimeType);
+
+  double averageAge = (age1 + age2) / 2;
+  print(averageAge);
+  print(averageAge.runtimeType);
+  print(averageAge.round());
+  print(averageAge.toInt());
 }
