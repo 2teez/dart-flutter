582a31287725ba1a8538da57384c5e07cd3ed31b
timothy adigun <adigun.timothy@gmail.com>
2026-08-29 17:01:57 +0100

    variables iin python and dart

---
+++ junkz/practice_variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  int dogs = 25;
+  dogs++;
+  print(dogs);
+  // 2
+  var age = 16;
+  print(age);
+  age = 30;
+  print(age);
+  // 30
+}
---
+++ junkz/practice_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python -OB
+#
+def main() -> None:
+    dogs = 25
+    dogs += 1
+    print(dogs)
+    # 26
+    age = 16
+    age = 30
+    print(age)
+    # 30
+
+
+if __name__ == "__main__":
+    main()
