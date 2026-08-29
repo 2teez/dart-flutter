8d4403fd9a03d06bf28949dd18ec1fb58b235176
timothy adigun <adigun.timothy@gmail.com>
2026-08-29 17:06:15 +0100

    using constant variables in dart

--- junkz/practice_variables.dart
+++ junkz/practice_variables.dart
@@ -8,4 +8,11 @@ void main() {
   age = 30;
   print(age);
   // 30
+  // exercise 4
+  const rating1 = 2.8;
+  const rating2 = 3.5;
+  const rating3 = 4.0;
+  const averageRating = (rating1 + rating2 + rating3) / 3;
+  print(averageRating);
+  //
 }
