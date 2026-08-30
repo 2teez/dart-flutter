515a6e3a5a5af200a51f88965a66dea2fff3f552
timothy adigun <adigun.timothy@gmail.com>
2026-08-30 09:50:41 +0100

    using grapheme cluster in the dart

--- fundamental-basics/ch04/practice_str.dart
+++ fundamental-basics/ch04/practice_str.dart
@@ -1,5 +1,15 @@
+import 'package:characters/characters.dart';
+
 void main() {
   String greeting = 'Hello, World!';
   print(greeting);
   print(greeting.codeUnits);
+  print(greeting.runes);
+  // using grapheme cluster
+  print(greeting.characters.length);
+  const flag = '🇳🇬';
+  print(flag.codeUnits);
+  print(flag.runes);
+  print(flag.characters);
+  print(flag.characters.length);
 }
