3d7ce9bbae40067588441ac88386612ef0c59cba
timothy adigun <adigun.timothy@gmail.com>
2026-09-03 09:49:46 +0100

    added thre name dart package

--- fundamental-basics/ch11/bin/main.dart
+++ fundamental-basics/ch11/bin/main.dart
@@ -1,6 +1,11 @@
+import 'package:ch11/name.dart';
 import 'package:ch11/user.dart';
 
 void main() {
   final user = User('Java');
   print(user);
+  final name = Name('Doe Java');
+  print(name);
+  final name2 = Name('Python');
+  print(name2);
 }
