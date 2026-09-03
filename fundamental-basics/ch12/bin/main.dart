d6a22522d7cb6dd6d331d9ab063388f7c1650c3b
timothy adigun <adigun.timothy@gmail.com>
2026-09-03 15:37:46 +0100

    check out list operations in dart

--- fundamental-basics/ch12/bin/main.dart
+++ fundamental-basics/ch12/bin/main.dart
@@ -25,4 +25,55 @@ void main() {
 void printed(String? msg, List<String>? months) {
   print(msg ?? '');
   print(months ?? []);
+  // exercise
+  const numbers = [1, 2, 4, 7];
+  for (var i = 0; i < numbers.length; i++) {
+    print(numbers[i] * numbers[i]);
+  }
+
+  for (final number in numbers) {
+    print(number * number);
+  }
+
+  final strings = const ['cookies', 'ice cream', 'cake', 'donuts', 'pie', 'brownies'];
+  int longestLength = 0;
+  String longestString = '';
+
+  int shortestLength = strings[0].length;
+  String shortestString = strings[0];
+
+  for (final string in strings) {
+    if (string.length > longestLength) {
+      longestLength = string.length;
+      longestString = string;
+    }
+    if (string.length < shortestLength) {
+      shortestLength = string.length;
+      shortestString = string;
+    }
+  }
+  print('Longest: $longestString, Shortest: $shortestString');
+  //
+  final stringsToSort = ['cookies', 'ice cream', 'cake', 'donuts', 'pie', 'brownies'];
+  for (var i = 0; i < stringsToSort.length; i++) {
+    for (var j = 0; j < stringsToSort.length; j++) {
+      if (stringsToSort[i].length < stringsToSort[j].length) {
+        final temp = stringsToSort[i];
+        stringsToSort[i] = stringsToSort[j];
+        stringsToSort[j] = temp;
+      }
+    }
+  }
+  print(stringsToSort);
+  //
+  final tobeSortedStrings = [
+    'cookies',
+    'ice cream',
+    'cake',
+    'donuts',
+    'pie',
+    'brownies',
+  ];
+  tobeSortedStrings.sort((a, b) => a.length.compareTo(b.length));
+  print(tobeSortedStrings);
 }
