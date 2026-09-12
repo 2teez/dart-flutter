24c0a9ab5571cad2ff460eccd3ca18a88f45037f
timothy adigun <adigun.timothy@gmail.com>
2026-09-12 02:41:31 +0100

    added a map generating function

--- fundamental-basics/ch14/lib/user.dart
+++ fundamental-basics/ch14/lib/user.dart
@@ -4,6 +4,15 @@ class User {
   final String _id;
   final String _name;
   final List<String> _emails;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic> {
+        "id": this._id,
+	"name": this._name,
+	"emails": this._emails,
+	};
+  }
+
   @override
   String toString() => 'User(id: $_id, name: $_name, emails: $_emails)';
 }
