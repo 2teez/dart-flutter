6b06a5ae2aff09d7dcf487033717fa93a545dc68
timothy adigun <adigun.timothy@gmail.com>
2026-09-12 03:27:37 +0100

    user dart file

--- fundamental-basics/ch14/lib/user.dart
+++ fundamental-basics/ch14/lib/user.dart
@@ -1,16 +1,18 @@
 class User {
   User({required this._id, required this._name, required this._emails});
+  factory User.fromJson(Map<String, dynamic> jsonMap) {
+    dynamic id = jsonMap['id'];
+    dynamic name = jsonMap['name'];
+    dynamic emails = jsonMap['emails'];
+    return User(id: id ?? "", name: name ?? "", emails: emails ?? []);
+  }
 
   final String _id;
   final String _name;
   final List<String> _emails;
 
   Map<String, dynamic> toJson() {
-    return <String, dynamic> {
-        "id": this._id,
-	"name": this._name,
-	"emails": this._emails,
-	};
+    return <String, dynamic>{"id": this._id, "name": this._name, "emails": this._emails};
   }
 
   @override
