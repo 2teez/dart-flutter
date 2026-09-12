174b4c153fb2c975933efaa853114e92306ee9a8
timothy adigun <adigun.timothy@gmail.com>
2026-09-12 18:05:10 +0100

    string manupilation in dart

---
+++ beyond_basics/ch00/str_man.dart
@@ -0,0 +1,8 @@
+void main() {
+  print('Hello, World!'.toLowerCase());
+  final timer = Duration(hours: 3, minutes: 9, seconds: 67);
+  final hrs = timer.inHours;
+  final mins = "${timer.inMinutes % 60}".padLeft(2, '0');
+  final secs = "${timer.inSeconds % 60}".padLeft(2, '0');
+  print("Time is: $hrs:$mins:$secs");
+}
