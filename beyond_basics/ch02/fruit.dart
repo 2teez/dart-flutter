f84ae09c6a00e50ec379273620f5406cee28e285
timothy adigun <adigun.timothy@gmail.com>
2026-09-13 20:51:05 +0100

    oop in dart

--- beyond_basics/ch02/fruit.dart
+++ beyond_basics/ch02/fruit.dart
@@ -2,6 +2,11 @@ void main() {
   final watermelon = Watermelon("red");
   watermelon.describeColor();
   print(watermelon);
+  // students
+  final std1 = Student([Roles.Athlete, Roles.StudentUnionMember]);
+  print(std1);
+  final std2 = Student([Roles.BandMember]);
+  print(std2);
 }
 
 class Fruit {
@@ -30,4 +35,16 @@ class Watermelon extends Melon {
 
 class Cantaloupe extends Melon {
   Cantaloupe(super.color);
+}
+
+enum Roles { Athlete, BandMember, StudentUnionMember }
+
+class Person {}
+
+class Student extends Person {
+  Student(this.roles);
+  final List<Roles> roles;
+
+  @override
+  String toString() => "Student(Roles: ${roles.join(', ')})";
 }
